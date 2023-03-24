9f084dd39abf62779ed4d1a027f050e1385b32df
mmartofel <mmartofe@redhat.com>
2023-03-24 21:36:26 +0100

    initial

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+
+<v-app>
+ <v-container>
+
+  <div>
+     <v-card class="mx-auto" width="500" prepend-icon="mdi-home">
+       <template v-slot:title>
+         Hello from Vuetify front powered by Nuxt 3
+       </template>
+       <v-card-text>
+         When you see this text inside a card, it worked!
+       </v-card-text>
+       <div>
+           <v-btn>Click the button</v-btn>
+       </div>
+     </v-card>
+  </div>
+ </v-container>
+</v-app>
+
+</template>
+
+ <script>
+ export default {
+ }
+ </script>
+
+ <style>
+ </style>
